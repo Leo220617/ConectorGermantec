4c930120a52e8714503fb959075e7c1463c2dc73
Leo220617 <32038650+Leo220617@users.noreply.github.com>
2021-06-29 14:45:11 -0600

    ca

---
+++ ConectorSAP/ConectorSAP/Home.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ConectorSAP
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerFacturas = new System.Windows.Forms.Timer(this.components);
+            this.lbErrores = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbEstadoActual = new System.Windows.Forms.Label();
+            this.timerSAPfacturas = new System.Windows.Forms.Timer(this.components);
+            this.timerInsertarInventario = new System.Windows.Forms.Timer(this.components);
+            this.timerInvVT = new System.Windows.Forms.Timer(this.components);
+            this.timerPrecios = new System.Windows.Forms.Timer(this.components);
+            this.checkBoxFac = new System.Windows.Forms.CheckBox();
+            this.InsercionFacturasSAP = new System.Windows.Forms.CheckBox();
+            this.InsertarInventario = new System.Windows.Forms.CheckBox();
+            this.InventarioVTEX = new System.Windows.Forms.CheckBox();
+            this.ActualizarPrecios = new System.Windows.Forms.CheckBox();
+            this.lbBitacora = new System.Windows.Forms.ListBox();
+            this.timerBodega = new System.Windows.Forms.Timer(this.components);
+            this.Bodegas = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timerFacturas
+            // 
+            this.timerFacturas.Interval = 50000;
+            this.timerFacturas.Tick += new System.EventHandler(this.timerFacturas_Tick);
+            // 
+            // lbErrores
+            // 
+            this.lbErrores.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbErrores.FormattingEnabled = true;
+            this.lbErrores.ItemHeight = 20;
+            this.lbErrores.Location = new System.Drawing.Point(24, 131);
+            this.lbErrores.Name = "lbErrores";
+            this.lbErrores.Size = new System.Drawing.Size(881, 124);
+            this.lbErrores.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Errores";
+            // 
+            // lbEstadoActual
+            // 
+            this.lbEstadoActual.AutoSize = true;
+            this.lbEstadoActual.Location = new System.Drawing.Point(21, 502);
+            this.lbEstadoActual.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbEstadoActual.Name = "lbEstadoActual";
+            this.lbEstadoActual.Size = new System.Drawing.Size(95, 17);
+            this.lbEstadoActual.TabIndex = 11;
+            this.lbEstadoActual.Text = "Estado Actual";
+            // 
+            // timerSAPfacturas
+            // 
+            this.timerSAPfacturas.Tick += new System.EventHandler(this.timerSAPfacturas_Tick);
+            // 
+            // timerInsertarInventario
+            // 
+            this.timerInsertarInventario.Tick += new System.EventHandler(this.timerInsertarInventario_Tick);
+            // 
+            // timerInvVT
+            // 
+            this.timerInvVT.Tick += new System.EventHandler(this.timerInvVT_Tick);
+            // 
+            // timerPrecios
+            // 
+            this.timerPrecios.Tick += new System.EventHandler(this.timerPrecios_Tick);
+            // 
+            // checkBoxFac
+            // 
+            this.checkBoxFac.AutoSize = true;
+            this.checkBoxFac.Location = new System.Drawing.Point(24, 21);
+            this.checkBoxFac.Name = "checkBoxFac";
+            this.checkBoxFac.Size = new System.Drawing.Size(146, 21);
+            this.checkBoxFac.TabIndex = 12;
+            this.checkBoxFac.Text = "Insercion Facturas";
+            this.checkBoxFac.UseVisualStyleBackColor = true;
+            this.checkBoxFac.CheckedChanged += new System.EventHandler(this.checkBoxFac_CheckedChanged);
+            // 
+            // InsercionFacturasSAP
+            // 
+            this.InsercionFacturasSAP.AutoSize = true;
+            this.InsercionFacturasSAP.Location = new System.Drawing.Point(189, 21);
+            this.InsercionFacturasSAP.Name = "InsercionFacturasSAP";
+            this.InsercionFacturasSAP.Size = new System.Drawing.Size(177, 21);
+            this.InsercionFacturasSAP.TabIndex = 13;
+            this.InsercionFacturasSAP.Text = "Insercion Facturas SAP";
+            this.InsercionFacturasSAP.UseVisualStyleBackColor = true;
+            this.InsercionFacturasSAP.CheckedChanged += new System.EventHandler(this.InsercionFacturasSAP_CheckedChanged);
+            // 
+            // InsertarInventario
+            // 
+            this.InsertarInventario.AutoSize = true;
+            this.InsertarInventario.Location = new System.Drawing.Point(392, 22);
+            this.InsertarInventario.Name = "InsertarInventario";
+            this.InsertarInventario.Size = new System.Drawing.Size(153, 21);
+            this.InsertarInventario.TabIndex = 14;
+            this.InsertarInventario.Text = "Insercion Inventario";
+            this.InsertarInventario.UseVisualStyleBackColor = true;
+            this.InsertarInventario.CheckedChanged += new System.EventHandler(this.InsertarInventario_CheckedChanged);
+            // 
+            // InventarioVTEX
+            // 
+            this.InventarioVTEX.AutoSize = true;
+            this.InventarioVTEX.Location = new System.Drawing.Point(569, 23);
+            this.InventarioVTEX.Name = "InventarioVTEX";
+            this.InventarioVTEX.Size = new System.Drawing.Size(132, 21);
+            this.InventarioVTEX.TabIndex = 15;
+            this.InventarioVTEX.Text = "Inventario VTEX";
+            this.InventarioVTEX.UseVisualStyleBackColor = true;
+            this.InventarioVTEX.CheckedChanged += new System.EventHandler(this.InventarioVTEX_CheckedChanged);
+            // 
+            // ActualizarPrecios
+            // 
+            this.ActualizarPrecios.AutoSize = true;
+            this.ActualizarPrecios.Location = new System.Drawing.Point(731, 23);
+            this.ActualizarPrecios.Name = "ActualizarPrecios";
+            this.ActualizarPrecios.Size = new System.Drawing.Size(143, 21);
+            this.ActualizarPrecios.TabIndex = 16;
+            this.ActualizarPrecios.Text = "Actualizar Precios";
+            this.ActualizarPrecios.UseVisualStyleBackColor = true;
+            this.ActualizarPrecios.CheckedChanged += new System.EventHandler(this.ActualizarPrecios_CheckedChanged);
+            // 
+            // lbBitacora
+            // 
+            this.lbBitacora.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBitacora.FormattingEnabled = true;
+            this.lbBitacora.ItemHeight = 20;
+            this.lbBitacora.Location = new System.Drawing.Point(24, 285);
+            this.lbBitacora.Name = "lbBitacora";
+            this.lbBitacora.Size = new System.Drawing.Size(881, 204);
+            this.lbBitacora.TabIndex = 17;
+            // 
+            // timerBodega
+            // 
+            this.timerBodega.Tick += new System.EventHandler(this.timerBodega_Tick);
+            // 
+            // Bodegas
+            // 
+            this.Bodegas.AutoSize = true;
+            this.Bodegas.Location = new System.Drawing.Point(24, 61);
+            this.Bodegas.Name = "Bodegas";
+            this.Bodegas.Size = new System.Drawing.Size(152, 21);
+            this.Bodegas.TabIndex = 18;
+            this.Bodegas.Text = "Actualizar Bodegas";
+            this.Bodegas.UseVisualStyleBackColor = true;
+            this.Bodegas.CheckedChanged += new System.EventHandler(this.Bodegas_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 265);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Bitacora";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 528);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Bodegas);
+            this.Controls.Add(this.lbBitacora);
+            this.Controls.Add(this.ActualizarPrecios);
+            this.Controls.Add(this.InventarioVTEX);
+            this.Controls.Add(this.InsertarInventario);
+            this.Controls.Add(this.InsercionFacturasSAP);
+            this.Controls.Add(this.checkBoxFac);
+            this.Controls.Add(this.lbEstadoActual);
+            this.Controls.Add(this.lbErrores);
+            this.Controls.Add(this.label1);
+            this.Name = "Home";
+            this.Text = "Middleware";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerFacturas;
+        private System.Windows.Forms.ListBox lbErrores;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbEstadoActual;
+        private System.Windows.Forms.Timer timerSAPfacturas;
+        private System.Windows.Forms.Timer timerInsertarInventario;
+        private System.Windows.Forms.Timer timerInvVT;
+        private System.Windows.Forms.Timer timerPrecios;
+        private System.Windows.Forms.CheckBox checkBoxFac;
+        private System.Windows.Forms.CheckBox InsercionFacturasSAP;
+        private System.Windows.Forms.CheckBox InsertarInventario;
+        private System.Windows.Forms.CheckBox InventarioVTEX;
+        private System.Windows.Forms.CheckBox ActualizarPrecios;
+        private System.Windows.Forms.ListBox lbBitacora;
+        private System.Windows.Forms.Timer timerBodega;
+        private System.Windows.Forms.CheckBox Bodegas;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ConectorSAP/ConectorSAP/Home.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Configuration;
+using System.IO;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using System.Xml.Serialization;
+ 
+using System.Xml;
+ 
+using System.Net.Http;
+using System.Web.Configuration;
+
+namespace ConectorSAP
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+            timerFacturas.Interval = Convert.ToInt32(ObtenerConfig("TiempoFacturasRecibir"));
+            timerSAPfacturas.Interval = Convert.ToInt32(ObtenerConfig("TiempoFacturasInsertar"));
+            timerInsertarInventario.Interval = Convert.ToInt32(ObtenerConfig("TiempoInventarioInsertar"));
+            timerInvVT.Interval = Convert.ToInt32(ObtenerConfig("TiempoInventarioInsertarVT"));
+            timerPrecios.Interval = Convert.ToInt32(ObtenerConfig("TiempoUpdatePrecios"));
+            timerBodega.Interval = Convert.ToInt32(ObtenerConfig("TiempoBodegasInvetarios"));
+
+
+
+        }
+
+        private void timerFacturas_Tick(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                Invoke(new Action(() => { timerFacturas.Enabled = false; }));
+                try
+                {
+                    AgregarMensajeEstado("Insertando Facturas", true);
+                    AgregarBitacora("Insertando Facturas...", "");
+
+                    HttpClient cliente = new HttpClient();
+                    string path = ObtenerConfig("urlInsercionFacturas");
+                    HttpResponseMessage response = await cliente.GetAsync(path);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        AgregarMensajeEstado("Finalizado", true);
+                    }
+                    else
+                    {
+                        throw new Exception(response.ReasonPhrase);
+                    }
+
+                    AgregarBitacora("Facturas Insertadas exitosamente...", "");
+
+                }
+                catch (Exception ex)
+                {
+
+                    AgregarError(ex, "Facturas ");
+                }
+
+                Invoke(new Action(() => { timerFacturas.Enabled = true; }));
+            });
+        }
+
+
+        private void AgregarError(Exception ex, string prefijo)
+        {
+            Invoke(new Action(() => { lbErrores.Items.Add(DateTime.Now.ToString("g") + " " + prefijo + ex.Message); Application.DoEvents(); }));
+        }
+
+        private void AgregarBitacora(string ex, string prefijo)
+        {
+            Invoke(new Action(() => { lbBitacora.Items.Add(DateTime.Now.ToString("g") + " " + prefijo + ex); Application.DoEvents(); }));
+        }
+
+
+        private void AgregarMensajeEstado(string mensaje, bool limpiar)
+        {
+            if (limpiar == true)
+            {
+              
+                Invoke(new Action(() => { lbEstadoActual.Text = ""; }));
+            }
+            else
+            {
+                mensaje = " " + mensaje;
+            }
+          
+
+            Invoke(new Action(() => { lbEstadoActual.Text += mensaje; Application.DoEvents(); }));
+
+            //Application.DoEvents();
+        }
+
+        public static string ObtenerConfig(string v)
+        {
+            try
+            {
+                return WebConfigurationManager.AppSettings[v];
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        private void timerSAPfacturas_Tick(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                Invoke(new Action(() => { timerSAPfacturas.Enabled = false; }));
+                try
+                {
+                    AgregarMensajeEstado("Mandando Facturas a SAP", true);
+                    AgregarBitacora("Insertando Facturas en SAP...", "");
+                    HttpClient cliente = new HttpClient();
+                    string path = ObtenerConfig("urlInsercionFacturasSAP");
+                    var httpContent = new StringContent("", Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await cliente.PostAsync(path, httpContent);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        AgregarMensajeEstado("Finalizado SAP", true);
+                    }
+                    else
+                    {
+                        throw new Exception(response.ReasonPhrase);
+                    }
+
+                    AgregarBitacora("Facturas Insertadas en SAP...", "");
+                }
+                catch (Exception ex)
+                {
+
+                    AgregarError(ex, "Facturas SAP ");
+                }
+
+                Invoke(new Action(() => { timerSAPfacturas.Enabled = true; }));
+            });
+        }
+
+        private void timerInsertarInventario_Tick(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                Invoke(new Action(() => { timerInsertarInventario.Enabled = false; }));
+                try
+                {
+                    AgregarMensajeEstado("Insertando Inventario", true);
+                    AgregarBitacora("Insertando Inventario...", "");
+                    HttpClient cliente = new HttpClient();
+                    string path = ObtenerConfig("urlInsertarInventario");
+                     
+                    HttpResponseMessage response = await cliente.GetAsync(path);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        AgregarMensajeEstado("Finalizado Inventario", true);
+                    }
+                    else
+                    {
+                        throw new Exception(response.ReasonPhrase);
+                    }
+
+                    AgregarBitacora("Insertado el inventario con éxito...", "");
+                }
+                catch (Exception ex)
+                {
+
+                    AgregarError(ex, "Insertar Inventario ");
+                }
+
+                Invoke(new Action(() => { timerInsertarInventario.Enabled = true; }));
+            });
+        }
+
+        private void timerInvVT_Tick(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                Invoke(new Action(() => { timerInsertarInventario.Enabled = false; }));
+                try
+                {
+                    AgregarMensajeEstado("Insertando Inventario en VTEX", true);
+                    AgregarBitacora("Insertando Inventario en VTEX...", "");
+                    HttpClient cliente = new HttpClient();
+                    string path = ObtenerConfig("urlInsertarInventarioVT");
+
+                    HttpResponseMessage response = await cliente.GetAsync(path);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        AgregarMensajeEstado("Finalizado Inventario VTEX", true);
+                    }
+                    else
+                    {
+                        throw new Exception(response.ReasonPhrase);
+                    }
+
+                    AgregarBitacora("Inventario insertado en VTEX...", "");
+                }
+                catch (Exception ex)
+                {
+
+                    AgregarError(ex, "Insertar Inventario VTEX ");
+                }
+
+                Invoke(new Action(() => { timerInsertarInventario.Enabled = true; }));
+            });
+        }
+
+        private void timerPrecios_Tick(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                Invoke(new Action(() => { timerInsertarInventario.Enabled = false; }));
+                try
+                {
+                    AgregarMensajeEstado("Actualizando Precios en VTEX", true);
+                    AgregarBitacora("Actualizando Precios en VTEX...", "");
+                    HttpClient cliente = new HttpClient();
+                    string path = ObtenerConfig("urlUpdatePrecios");
+
+                    HttpResponseMessage response = await cliente.GetAsync(path);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        AgregarMensajeEstado("Finalizado Precios VTEX", true);
+                    }
+                    else
+                    {
+                        throw new Exception(response.ReasonPhrase);
+                    }
+
+                    AgregarBitacora("Precios Actualizados en VTEX...", "");
+                }
+                catch (Exception ex)
+                {
+
+                    AgregarError(ex, "Actualizar Precios VTEX ");
+                }
+
+                Invoke(new Action(() => { timerInsertarInventario.Enabled = true; }));
+            });
+        }
+
+        private void checkBoxFac_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBoxFac.Checked)
+            {
+                timerFacturas.Enabled = true;
+            }
+            else
+            {
+                timerFacturas.Enabled = false;
+            }
+        }
+
+        private void InsercionFacturasSAP_CheckedChanged(object sender, EventArgs e)
+        {
+            if (InsercionFacturasSAP.Checked)
+            {
+                timerSAPfacturas.Enabled = true;
+            }
+            else
+            {
+                timerSAPfacturas.Enabled = false;
+            }
+        }
+
+        private void InsertarInventario_CheckedChanged(object sender, EventArgs e)
+        {
+            if (InsertarInventario.Checked)
+            {
+                timerInsertarInventario.Enabled = true;
+            }
+            else
+            {
+                timerInsertarInventario.Enabled = false;
+            }
+        }
+
+        private void InventarioVTEX_CheckedChanged(object sender, EventArgs e)
+        {
+            if (InventarioVTEX.Checked)
+            {
+                timerInvVT.Enabled = true;
+            }
+            else
+            {
+                timerInvVT.Enabled = false;
+            }
+        }
+
+        private void ActualizarPrecios_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ActualizarPrecios.Checked)
+            {
+                timerPrecios.Enabled = true;
+            }
+            else
+            {
+                timerPrecios.Enabled = false;
+            }
+
+        }
+
+        private void timerBodega_Tick(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                Invoke(new Action(() => { timerBodega.Enabled = false; }));
+                try
+                {
+                    AgregarMensajeEstado("Actualizando Bodegas", true);
+                    AgregarBitacora("Actualizando Bodegas...", "");
+                    HttpClient cliente = new HttpClient();
+                    string path = ObtenerConfig("urlBodegasInventarios");
+
+                    HttpResponseMessage response = await cliente.GetAsync(path);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        AgregarMensajeEstado("Finalizado Bodegas", true);
+                    }
+                    else
+                    {
+                        throw new Exception(response.ReasonPhrase);
+                    }
+
+                    AgregarBitacora("Finalizado Bodegas...", "");
+                }
+                catch (Exception ex)
+                {
+
+                    AgregarError(ex, "Actualizar Bodegas ");
+                }
+
+                Invoke(new Action(() => { timerBodega.Enabled = true; }));
+            });
+        }
+
+        private void Bodegas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Bodegas.Checked)
+            {
+                timerBodega.Enabled = true;
+            }
+            else
+            {
+                timerBodega.Enabled = false;
+            }
+        }
+    }
+}
